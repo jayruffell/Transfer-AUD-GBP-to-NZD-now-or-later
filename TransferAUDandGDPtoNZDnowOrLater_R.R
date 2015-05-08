2a59b84c523f5055ce8de18f397281f46909f269
jayruffell <ruffell.jay@gmail.com>
2015-05-09 11:41:13 +1200

    test commit

---
+++ TransferAUDandGDPtoNZDnowOrLater_R.R
@@ -0,0 +1,16 @@
+#this is a test
+
+testinput <- 1:100
+testVec <- vector()
+
+for(i in 1:100){
+  testVec[i] <- testinput[i]*2
+  #print([i])
+}
+
+testVec
+
+testVec <-1:10
+for(i in 1:10){
+  print(testVec[i])
+}
