6c0f2b21c70e11ff7b8fde393259197f5fec2749
jayruffell <ruffell.jay@gmail.com>
2015-05-09 12:00:41 +1200

    master, or new branch? not sure where this iwll end up

--- TransferAUDandGDPtoNZDnowOrLater_R.R
+++ TransferAUDandGDPtoNZDnowOrLater_R.R
@@ -7,8 +7,10 @@ for(i in 1:100){
   testVec[i] <- testinput[i]^2
   #print([i])
 }
-
+t
 #now i'm changing the file by addin gthis comment, and uploading (pushing) without 'committing'. should come up as a separate
 #commit to the earlier version.
 #==> actually no, 'pushing' without first 'committing' seems to do nothing. I think you have to C then P, and when you do this
 #the different versions of the file come up in the committ's history.
+
+#now, these comments are after adding a new branch on github - so am I now typing on the master, or the 'new branch'?
