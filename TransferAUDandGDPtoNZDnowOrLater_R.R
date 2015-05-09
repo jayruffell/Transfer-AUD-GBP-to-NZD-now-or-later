ee806758d1721dd2b80a851a2a36a37391e896d1
jayruffell <ruffell.jay@gmail.com>
2015-05-09 12:05:17 +1200

    deleted a typo at line 10

--- TransferAUDandGDPtoNZDnowOrLater_R.R
+++ TransferAUDandGDPtoNZDnowOrLater_R.R
@@ -7,7 +7,7 @@ for(i in 1:100){
   testVec[i] <- testinput[i]^2
   #print([i])
 }
-t
+
 #now i'm changing the file by addin gthis comment, and uploading (pushing) without 'committing'. should come up as a separate
 #commit to the earlier version.
 #==> actually no, 'pushing' without first 'committing' seems to do nothing. I think you have to C then P, and when you do this
